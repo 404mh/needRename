1ba198c5d7b826e842ab77006e5a3312acac7f3b
dahyun <dhdh3311@naver.com>
2021-03-29 00:40:20 +0900

    feat: add header(logo, profile section)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import styles from "./index.module.scss";
 import React from "react";
 
 export default function Home(): React.ReactElement {
-  return <></>;
+  return (
+    <div className={styles.root}>
+      <header className={styles.logo_profile}>
+        <h1>TIL</h1>
+        <div>profile</div>
+      </header>
+    </div>
+  );
 }
